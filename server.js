da088bf1dc1e8c9d4cc5a64bda368c2c8e0fbe45
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-24 16:25:56 +0200

    lägger till mongoose schema + routes för get, post, put

--- server.js
+++ server.js
@@ -16,16 +16,103 @@ app.use(cors());
 app.use(express.json());
 
 //Anslutning databas
-mongoose.connect("").then(() => {
+mongoose.connect("mongodb://localhost:27017/lab3").then(() => {
     console.log("Conntected to database");
 }).catch((error) => {
     console.log("Connection failure" + error);
 })
 
-//Routes
-app.get("/api", async (req, res) => {
+//Schema
+const schema = new mongoose.Schema({
+    companyname: {
+        type: String,
+        required: true
+    },
+    jobtitle: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    startdate: {
+        type: String,
+        required: true
+    },
+    enddate: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
 
+//Applicera schema på collection
+const Job = mongoose.model("Job", schema);
+
+//Routes
+app.get("/", async (req, res) => {
+    res.json({ message: `Welcome to API for jobs` });
 });
+
+app.get("/jobs", async (req, res) => {
+    try {
+        let result = await Job.find({}); //Hämta all data'
+
+        //kontroll om databas saknar data
+        if (result.length === 0) {
+            res.status(404).json({ message: "No jobs found" });
+        } else {
+            return res.json(result); //returnera resonse med data
+        }
+    } catch (error) {
+        return res.status(500).json(error); // felmeddelande
+    }
+});
+
+app.get("/jobs/:id", async (req, res) => {
+
+    try {
+        let result = await Job.find({ _id: req.params.id });
+
+        return res.json(result);
+    } catch (error) {
+        return res.status(400).json({ message: `ID not found. Error-message: ${error}` });
+    }
+})
+
+app.post("/jobs", async (req, res) => {
+    try {
+        let result = await Job.create(req.body);
+        return res.json(result);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+})
+
+app.put("/jobs/:id", async (req, res) => {
+    const query = { _id: req.params.id };
+    const update = {
+        $set: {
+            companyname: req.body.companyname,
+            jobtitle: req.body.jobtitle,
+            location: req.body.location,
+            description: req.body.description,
+            startdate: req.body.startdate,
+            enddate: req.body.enddate
+        }
+    };
+
+    try {
+        let result = await Job.updateOne(query, update);
+        return res.json({ message: `Success! Job with ID: ${req.params.id} is now changed.` });
+    } catch (error) {
+        return res.status(400).json({ message: `ID not found. Error-message: ${error}` });
+    }
+})
 
 //starta server
 app.listen(port, () => {
